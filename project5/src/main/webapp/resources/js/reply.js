6ddadca85a8d179a19d39d4be5d81e5c9d472e4d
devbeta8t3 <sehoan30@gmail.com>
2022-10-20 16:13:22 +0900

    no changes

--- project5/src/main/webapp/resources/js/reply.js
+++ project5/src/main/webapp/resources/js/reply.js
@@ -106,25 +106,26 @@ let replyService = (function(){
 		});
 	}
 	
-	// 시간 변환, 24시간이 지나지 않으면 시분초 표시, 지나면 날짜 표시
+	// 시간 표기 변환, 24시간이 지나지 않으면 시분초 표시, 지나면 날짜 표시
 	function displayTime(timeValue){
-		var today = new Date();
+		let today = new Date();
 		
-		var gap = today.getTime() - timeValue;
+		let gap = today.getTime() - timeValue;
 		
-		var dateObj = new Date(timeValue);
-		var str = "";
+		let dateObj = new Date(timeValue);
+		let str = "";
 		
 		if(gap < (1000 * 60 * 60 * 24)){
-			var hh = dateObj.getHours();
-			var mi = dateObj.getMinutes();
-			var ss = dateObj.getSeconds();
+			let hh = dateObj.getHours();
+			let mi = dateObj.getMinutes();
+			let ss = dateObj.getSeconds();
 			
 			return [(hh>9 ? '' : '0') + hh, ':', (mi > 9 ? '' : '0') + mi, ':', (ss > 9 ? '' : '0') + ss].join('');
-		}else{
-			var yy = dateObj.getFullYear();
-			var mm = dateObj.getMonth() + 1; // getMonth() is zero-based
-			var dd = dateObj.getDate();
+		}
+		else{
+			let yy = dateObj.getFullYear();
+			let mm = dateObj.getMonth() + 1; // getMonth() is zero-based
+			let dd = dateObj.getDate();
 			
 			return [yy, '/', (mm > 9 ? '' : '0')+mm, '/', (dd > 9 ? '' : '0') + dd].join('');
 		}
